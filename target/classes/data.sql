cd878bb9e8e5f6df3b0b2af9c253d1d903421639
Tom <tom8034434@gmail.com>
2023-12-14 23:19:00 +0800

    Remove classes folder

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO def_currency (currency_code, description) VALUES ('USD', '美元');
-INSERT INTO def_currency (currency_code, description) VALUES ('GBP', '英鎊');
-INSERT INTO def_currency (currency_code, description) VALUES ('EUR', '歐元');
